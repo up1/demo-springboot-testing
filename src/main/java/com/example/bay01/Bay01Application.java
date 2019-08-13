ec32c8197667666a67b52ea8e0c1a5a315931f03
up1 <somkiat.p@gmail.com>
2019-08-13 21:47:29 +0700

    Starting demo

---
+++ src/main/java/com/example/bay01/Bay01Application.java
@@ -0,0 +1,13 @@
+package com.example.bay01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bay01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bay01Application.class, args);
+	}
+
+}
