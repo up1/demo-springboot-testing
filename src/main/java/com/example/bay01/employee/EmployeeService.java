38f4f29f45fadc08fa13c84dc006203c9351d5ad
up1 <somkiat.p@gmail.com>
2019-08-13 22:51:31 +0700

    Fix springboot testing with mockbean

--- src/main/java/com/example/bay01/employee/EmployeeService.java
+++ src/main/java/com/example/bay01/employee/EmployeeService.java
@@ -1,12 +1,19 @@
 package com.example.bay01.employee;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     public EmployeeResponse getBy(int id) {
-        return new EmployeeResponse(1, "Somkiat Pui");
+        Optional<Employee> employee = employeeRepository.findById(id);
+        return new EmployeeResponse(1, String.format("%s %s", employee.get().getFirstName(), employee.get().getLastName()));
     }
 
 }
--- src/test/java/com/example/bay01/employee/EmployeeControllerSpringBootTest.java
+++ src/test/java/com/example/bay01/employee/EmployeeControllerSpringBootTest.java
@@ -4,10 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.boot.test.context.SpringBootTest.*;
 
 @RunWith(SpringRunner.class)
@@ -17,8 +19,15 @@ public class EmployeeControllerSpringBootTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @MockBean
+    private EmployeeService employeeService;
+
     @Test
     public void success_getEmployee_id_1() {
+        // Arrange
+        given(this.employeeService.getBy(1))
+                .willReturn(new EmployeeResponse(1, "Somkiat Pui"));
+
         // Act
         EmployeeResponse response = restTemplate.getForObject("/employee/1", EmployeeResponse.class);
 
