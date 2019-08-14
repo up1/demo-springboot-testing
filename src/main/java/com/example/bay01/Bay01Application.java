af6359cc1482f943aca045ef220b6426f8b3b560
up1 <somkiat.p@gmail.com>
2019-08-14 09:22:14 +0700

    Working with API

--- src/main/java/com/example/bay01/Bay01Application.java
+++ src/main/java/com/example/bay01/Bay01Application.java
@@ -2,12 +2,20 @@ package com.example.bay01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Bay01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Bay01Application.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplateBuilder().build();
 	}
 
 }
--- src/main/java/com/example/bay01/employee/EmployeeController.java
+++ src/main/java/com/example/bay01/employee/EmployeeController.java
@@ -1,9 +1,13 @@
 package com.example.bay01.employee;
 
+import com.example.bay01.post.PostGateway;
+import com.example.bay01.post.PostResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class EmployeeController {
@@ -11,9 +15,17 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
+    @Autowired
+    private PostGateway postGateway;
+
     @GetMapping("/employee/{id}")
     public EmployeeResponse getEmployee(@PathVariable int id) {
         return employeeService.getBy(id);
+    }
+
+    @GetMapping("/post/{id}")
+    public Optional<PostResponse> getPost(@PathVariable int id) {
+        return postGateway.getPostBy(id);
     }
 
 }
